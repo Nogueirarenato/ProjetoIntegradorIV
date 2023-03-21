b400ecd943683b3fe5e958203933d776182e87c6
Renato Nogueira <renatonogueirasenai@gmail.com>
2023-03-20 22:35:32 -0300

    up

---
+++ drives/Drives.js
@@ -0,0 +1,25 @@
+const Sequelize = require('sequelize');
+const connection = require('../database/database');
+
+
+
+
+
+const Drives = connection.define('drives', {
+    id_button: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+    },
+    temperature: {
+        type: Sequelize.FLOAT,
+        allowNull: false
+    }  
+    
+})
+
+
+Drives.sync({force: true});
+
+module.exports = Drives;
+
+
